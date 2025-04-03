d24621f25ffaaf5784ade5effad0ae0fcf4cb15d
alepek <5012939+alepek@users.noreply.github.com>
2025-04-03 13:15:58 +0200

    heyo

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,30 +1,58 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from "next/font/google";
 
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/providers"
+import "@workspace/ui/globals.css";
+import { Toaster } from "@workspace/ui/components/sonner";
+import { Providers } from "@/components/providers";
+import {
+  NavigationMenu,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@workspace/ui/components/navigation-menu";
+import Link from "next/link";
+import { DarkModeToggle } from "@/components/dark-mode";
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <main className="max-w-6xl mx-auto">
+            <div className="p-4 pb-8 flex justify-between">
+              <NavigationMenu>
+                <Link href="/" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Home
+                  </NavigationMenuLink>
+                </Link>
+                <Link href="/help" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Help
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenu>
+              <DarkModeToggle />
+            </div>
+            {children}
+          </main>
+        </Providers>
+        <Toaster />
       </body>
     </html>
-  )
+  );
 }
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,19 @@
-import { Button } from "@workspace/ui/components/button"
+"use client";
+import { ChartExample } from "@/components/chart";
+import { Button } from "@workspace/ui/components/button";
+import Link from "next/link";
+import { toast } from "sonner";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex items-center justify-center max-w-lg mx-auto">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-2xl font-bold">The home page</h1>
+        <Button size="sm" onClick={() => toast("Hey hey")}>
+          Sonner
+        </Button>
+        <ChartExample />
       </div>
     </div>
-  )
+  );
 }

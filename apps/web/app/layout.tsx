56dbcd5e6f1524028b3cdd5a6a99468d34cc7438
alepek <5012939+alepek@users.noreply.github.com>
2025-04-03 13:25:22 +0200

    gap in menu

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
         <Providers>
           <main className="max-w-6xl mx-auto">
             <div className="p-4 pb-8 flex justify-between">
-              <NavigationMenu>
+              <NavigationMenu className="flex gap-2">
                 <Link href="/" legacyBehavior passHref>
                   <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                     Home

56413cd25afbd466e8aa18390240a07f2009f138
alepek <5012939+alepek@users.noreply.github.com>
2025-04-08 12:49:10 +0200

    fix api route

--- apps/web/app/api/blog/[id]/route.ts
+++ apps/web/app/api/blog/[id]/route.ts
@@ -2,9 +2,10 @@ import { Post } from "@/components/blog-post";
 
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
-  const data = await fetch(`https://api.vercel.app/blog/${params.id}`);
+  const { id } = await params;
+  const data = await fetch(`https://api.vercel.app/blog/${id}`);
   const post: Post[] = await data.json();
 
   return Response.json({ post });

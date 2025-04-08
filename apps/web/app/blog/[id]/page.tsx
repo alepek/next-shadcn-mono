459f92d30f68603a3640a5925819932fc3c72b2d
alepek <5012939+alepek@users.noreply.github.com>
2025-04-08 12:16:55 +0200

    async params

--- apps/web/app/blog/[id]/page.tsx
+++ apps/web/app/blog/[id]/page.tsx
@@ -1,8 +1,13 @@
-export default function BlogPostPage({ params }: { params: { id: string } }) {
+export default async function BlogPostPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   return (
     <div className="mx-auto w-full">
       <h2 className="text-4xl font-semibold tracking-tight text-pretty sm:text-5xl">
-        Post {params.id}
+        Post {id}
       </h2>
       <p className="mt-2 text-lg/8 ">
         Hipster ipsum dolor sit amet, consectetur adipiscing elit. Nullam in

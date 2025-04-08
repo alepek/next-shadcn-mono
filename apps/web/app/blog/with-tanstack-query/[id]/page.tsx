8ca6112e3951b2c167645e4052116533b0fdf24a
alepek <5012939+alepek@users.noreply.github.com>
2025-04-08 13:03:54 +0200

    responsive loading state

--- apps/web/app/blog/with-tanstack-query/[id]/page.tsx
+++ apps/web/app/blog/with-tanstack-query/[id]/page.tsx
@@ -18,8 +18,8 @@ export default function BlogPostPage() {
   if (isLoading) {
     return (
       <article className="w-full">
-        <div className="animate-pulse h-8 w-84 bg-accent rounded-md"></div>
-        <div className="animate-pulse h-6 mt-5 w-128 bg-accent rounded-md"></div>
+        <div className="animate-pulse h-8 w-full md:w-84 bg-accent rounded-md"></div>
+        <div className="animate-pulse h-6 mt-5 w-full md:w-128 bg-accent rounded-md"></div>
       </article>
     );
   }

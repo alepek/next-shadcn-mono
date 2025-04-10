1ce15d7c0a3c8ec5e6ded0ced852770d3368c430
alepek <5012939+alepek@users.noreply.github.com>
2025-04-10 11:08:35 +0200

    remove some dead code

--- apps/web/trpc/server.tsx
+++ apps/web/trpc/server.tsx
@@ -5,7 +5,6 @@ import {
 } from "@trpc/tanstack-react-query";
 import { cache } from "react";
 import { makeQueryClient } from "./query-client";
-import { createTRPCClient, httpLink } from "@trpc/client";
 import { appRouter } from "./router";
 
 // IMPORTANT: Create a stable getter for the query client that
@@ -15,13 +14,5 @@ export const getQueryClient = cache(makeQueryClient);
 export const trpc = createTRPCOptionsProxy({
   ctx: createTRPCContext,
   router: appRouter,
-  queryClient: getQueryClient,
-});
-
-// If your router is on a separate server, pass a client:
-createTRPCOptionsProxy({
-  client: createTRPCClient({
-    links: [httpLink({ url: "..." })],
-  }),
   queryClient: getQueryClient,
 });

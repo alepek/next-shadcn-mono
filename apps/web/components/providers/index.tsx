895433fe1e8538d34c998891d7e8a3de6f9f647e
alepek <5012939+alepek@users.noreply.github.com>
2025-04-10 13:43:26 +0200

    re-add devtools

--- apps/web/components/providers/index.tsx
+++ apps/web/components/providers/index.tsx
@@ -1,10 +1,12 @@
 import { TRPCReactProvider } from "@/trpc/client";
 import { AppThemeProvider } from "./theme";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <TRPCReactProvider>
       <AppThemeProvider>{children}</AppThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </TRPCReactProvider>
   );
 }
